95eb230e7e8d7d4a9ea4af06bb16cc7538c7b06f
div468 <julianandre.divas@gmail.com>
2026-04-23 01:25:22 -0600

    Add: Ratings validation

---
+++ internal/utils/ratings_validation.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"errors"
+	"backend/internal/models"
+)
+
+func ValidateRatings(rating models.Ratings) error {
+	if rating.StarsQuantity < 1 || rating.StarsQuantity > 5 {
+		return errors.New("stars must be a value between 1 and 5")
+	}
+
+	if rating.SeriesID <= 0 {
+		return errors.New("series_id is required")
+	}
+
+	if len(rating.Content) > 500 {
+		return errors.New("content is too long")
+	}
+
+	return nil
+}

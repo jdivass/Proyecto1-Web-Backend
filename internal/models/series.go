40d4176ce21d8ec18684685bcfba159f14bad319
div468 <julianandre.divas@gmail.com>
2026-04-22 22:03:36 -0600

    feat: Add database initialization and seed data

---
+++ internal/models/series.go
@@ -0,0 +1,23 @@
+package models
+import (
+	"time"
+)
+
+type Series struct {
+	ID int `json:"id"`
+	Title string `json:"title"`
+	Genre string `json:"genre"`
+	Description string `json:"description"`
+	Platform string `json:"platform"`
+	Status int `json:"status"`
+	ImagePath string `json:"image_path"`
+
+	TotalSeasons int `json:"total_seasons"`
+	TotalEpisodes int `json:"total_episodes"`
+
+	CurrentSeason int `json:"current_season"`
+	CurrentEpisode int `json:"current_episode"`
+ 
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
